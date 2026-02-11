688753a8835c9df20bea5b5f57f49782fffb6a9f
Karthik B <karthikbalu5533@gmail.com>
2026-02-11 21:32:27 +0530

    changed to netflix theme....

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -8,22 +8,24 @@ const UserDetail = () => {
   const user = users.find((u) => u.id === parseInt(id));
 
   if (!user) {
-    return <h2 className="text-center mt-20 text-red-500">User Not Found</h2>;
+    return <h2 className="text-center mt-20 text-red-600">User Not Found</h2>;
   }
 
   return (
-    <div className="max-w-md mx-auto mt-20 bg-white shadow-xl rounded-xl p-8 text-center">
-      <h2 className="text-2xl font-bold text-indigo-600 mb-4">
-        {user.name}
-      </h2>
-      <p className="text-gray-600 mb-6">{user.email}</p>
+    <div className="flex justify-center items-center h-[70vh]">
+      <div className="bg-gray-900 p-10 rounded-xl shadow-xl text-center">
+        <h2 className="text-3xl font-bold mb-4 text-red-600">
+          {user.name}
+        </h2>
+        <p className="text-gray-400 mb-6">{user.email}</p>
 
-      <button
-        onClick={() => navigate("/users")}
-        className="bg-purple-600 text-white px-6 py-2 rounded-lg hover:bg-purple-700 transition"
-      >
-        Go Back
-      </button>
+        <button
+          onClick={() => navigate("/users")}
+          className="bg-red-600 px-6 py-2 rounded hover:bg-red-700 transition"
+        >
+          Go Back
+        </button>
+      </div>
     </div>
   );
 };

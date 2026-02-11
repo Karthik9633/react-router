b3255b9c5e470a12e6756bd495a8bf6c26f044aa
Karthik B <karthikbalu5533@gmail.com>
2026-02-11 21:31:53 +0530

    changed to netflix theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,25 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[80vh] text-center">
-      <h2 className="text-4xl font-bold text-indigo-600 mb-4">
+    <div className="h-[80vh] flex flex-col justify-center items-center text-center px-6">
+      <h2 className="text-5xl font-bold mb-6">
         Welcome to React Router Website
       </h2>
-      <p className="text-gray-600 mb-6">
+      <p className="text-gray-400 max-w-xl mb-8">
         This application demonstrates routing and dynamic pages using React Router.
       </p>
 
       <div className="space-x-4">
         <Link
           to="/about"
-          className="bg-indigo-600 text-white px-6 py-2 rounded-lg shadow hover:bg-indigo-700 transition"
+          className="border border-gray-500 px-6 py-3 rounded hover:border-white transition"
         >
           About
         </Link>
 
         <Link
           to="/users"
-          className="bg-purple-600 text-white px-6 py-2 rounded-lg shadow hover:bg-purple-700 transition"
+          className="bg-red-600 px-6 py-3 rounded hover:bg-red-700 transition"
         >
           Users
         </Link>
